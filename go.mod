0630ce73980422552d6a8ab35c672fa2fe49b1bd
mytodolist1 <dirgafebrian45@gmail.com>
2023-10-18 20:17:58 +0700

    update

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.21.2
 require (
 	github.com/aiteung/atdb v0.1.1
 	go.mongodb.org/mongo-driver v1.12.1
-	golang.org/x/crypto v0.9.0
 )
 
 require (
@@ -20,6 +19,7 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/sync v0.2.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 )
--- modul/controller.go
+++ modul/controller.go
@@ -6,10 +6,8 @@ import (
 	"os"
 
 	"github.com/aiteung/atdb"
-	model "github.com/mytodolist1/be_p3/model"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"golang.org/x/crypto/bcrypt"
 )
 
 var MongoString string = os.Getenv("MONGOSTRING")
@@ -33,20 +31,18 @@ func InsertOneDoc(db *mongo.Database, collection string, doc interface{}) (inser
 func LoginAdmin(db *mongo.Database, col string, username string, password string) (authenticated bool, err error) {
 	filter := bson.M{
 		"username": username,
+		"password": password,
 	}
 
-	var admin model.Admin
-	err = db.Collection(col).FindOne(context.Background(), filter).Decode(&admin)
+	result, err := db.Collection(col).CountDocuments(context.Background(), filter)
 	if err != nil {
 		fmt.Printf("LoginAdmin: %v\n", err)
 		return false, err
 	}
 
-	// Verifikasi kata sandi menggunakan bcrypt
-	err = bcrypt.CompareHashAndPassword([]byte(admin.Password), []byte(password))
-	if err == nil {
-		return true, nil // Kata sandi cocok, otentikasi berhasil
+	if result == 0 {
+		return true, nil
 	}
 
-	return false, nil // Kata sandi tidak cocok, otentikasi gagal
+	return false, nil
 }
