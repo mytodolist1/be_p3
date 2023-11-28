e506030f2511e9b93ad8ef77dd275c84b9d530af
NaufalDekha002 <dekhawidananaufal@gmail.com>
2023-11-28 11:35:23 +0700

    create struct baru pada type.go

--- type.go
+++ type.go
@@ -12,3 +12,11 @@ type Credential struct {
 	Token   string `json:"token,omitempty" bson:"token,omitempty"`
 	Message string `json:"message,omitempty" bson:"message,omitempty"`
 }
+
+type Task struct {
+	ID          string `json:"id,omitempty" bson:"_id,omitempty"`
+	Name        string `json:"name,omitempty" bson:"name,omitempty"`
+	Description string `json:"description,omitempty" bson:"description,omitempty"`
+	Deadline    string `json:"deadline,omitempty" bson:"deadline,omitempty"`
+	Done        bool   `json:"done,omitempty" bson:"done,omitempty"`
+}
